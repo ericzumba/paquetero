bbb712f6c88da1440eded4b555fee28dba1a686a
Eric Torti <eric.torti@vivareal.com>
2016-10-07 15:48:43 -0300

    retry parameters

--- run.py
+++ run.py
@@ -11,12 +11,15 @@ def cli():
 @click.option('--port', default=8983, help='Solr port')
 @click.option('--core', help='Solr core to be backed up')
 @click.option('--location', help='Disk location to which solr will dump the backup ')
-def backup(host, port, core, location):
+@click.option('--max-retries', default=30, help='Maximum times paquetero checks if backup is ready')
+@click.option('--time-between-retries', default=5, help='Time in seconds between each "backup is ready?" request')
+def backup(host, port, core, location, max_retries, time_between_retries):
   click.echo('Requesting backup')
   core_location = "{0}/{1}".format(location, core)
   resp = requests.get('http://{0}:{1}/solr/{2}/replication?command=backup&location=${3}'.format(host, port, core, core_location))
   click.echo('Backup requested')
-  while True:
+
+  while True and max_retries > 0:
     click.echo('Checking if backup is ready')
     r = requests.get('http://{0}:{1}/solr/{2}/replication?command=restorestatus'.format(host, port, core))
     if "No restore actions in progress" in r.content:
@@ -24,7 +27,8 @@ def backup(host, port, core, location):
       move_backup('a', 'b')
       break
     else: 
-      time.sleep(5) 
+      time.sleep(time_between_retries) 
+    max_retries = max_retries - 1
 
 def move_backup(source, target):
   click.echo('Moving backup to go')
