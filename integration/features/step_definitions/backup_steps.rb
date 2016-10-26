60c4af21e06d1d9c3363c5a60a1410c0306340b6
Eric Torti <eric.torti@vivareal.com>
2016-10-26 16:48:17 -0200

    dealing with solr core creation

--- integration/features/step_definitions/backup_steps.rb
+++ integration/features/step_definitions/backup_steps.rb
@@ -1,11 +1,15 @@
 require 'rsolr'
+require 'fileutils'
 
 Given(/^solr is running$/) do
 	@solr = RSolr.connect :url => 'http://solr:8983/solr'	
 end
 
 Given(/^it has a core named books$/) do
-	@solr.get 'admin/cores', params: { action: 'CREATE', name: 'books' }
+  FileUtils.mkdir_p '/opt/solr/server/solr/books'
+  insist(5) do
+    @solr.get 'admin/cores', params: { action: 'CREATE', name: 'books' }
+  end
 end
 
 Given(/^there is one indexed document$/) do
