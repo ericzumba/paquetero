816b89043a7207a7b1da8f9ad931a9ccbf514a8c
Eric Torti <eric.torti@vivareal.com>
2016-10-26 18:15:43 -0200

    insisting til solr is up

--- integration/features/step_definitions/backup_steps.rb
+++ integration/features/step_definitions/backup_steps.rb
@@ -2,14 +2,15 @@ require 'rsolr'
 require 'fileutils'
 
 Given(/^solr is running$/) do
-	@solr = RSolr.connect :url => 'http://solr:8983/solr'	
+  insist(5) do # Solr takes some time to start
+    @solr = RSolr.connect :url => 'http://solr:8983/solr'	
+    @solr.get '/'
+  end
 end
 
 Given(/^it has a core named books$/) do
   FileUtils.mkdir_p '/opt/solr/server/solr/books'
-  insist(5) do
-    @solr.get 'admin/cores', params: { action: 'CREATE', name: 'books' }
-  end
+  @solr.get 'admin/cores', params: { action: 'CREATE', name: 'books' }
 end
 
 Given(/^there is one indexed document$/) do
