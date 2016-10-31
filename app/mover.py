71410001ab9baaac75627b97366ce28eb3133014
Eric Torti <eric.torti@vivareal.com>
2016-10-31 15:50:52 -0200

    working

--- app/mover.py
+++ app/mover.py
@@ -2,18 +2,23 @@ import tinys3
 import click
 import shutil
 from os import environ 
+import boto
+from boto.s3.connection import S3Connection
 
 aws_id = environ['AWS_ACCESS_KEY_ID']
 aws_secret = environ['AWS_SECRET_ACCESS_KEY']
 
 def s3(region):
-  conn = tinys3.Connection(aws_id, aws_secret, tls=True, endpoint='s3-{0}.amazonaws.com'.format(region))
+  conn = S3Connection(aws_id, aws_secret) 
   
   def store(bucket_name, source):
     tar = '{0}.tar'.format(source)
     shutil.make_archive(source, 'tar', source)
-    click.echo('starting copy of {0} to s3 {1}'.format(tar, bucket_name))
-    conn.upload(tar, open(tar, 'rb'), bucket_name)
-    click.echo('copy completed')
+    click.echo('Starting copy of {0} to s3 {1}'.format(tar, bucket_name))
+    bucket = conn.get_bucket(bucket_name)
+    key = boto.s3.key.Key(bucket, 'teste.tar')
+    with open(tar, 'rb') as f:
+      key.set_contents_from_file(f)
+    click.echo('Copy completed')
 
   return dict(store=store) 
